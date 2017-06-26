695776fe0ec8f90dd8852c92238b695e6299afc7
海老原祐輔 <ebiyuu1121@gmail.com>
2017-06-26 15:03:20 +0900

    made files: main.h, calcutale.h

---
+++ makenumber/calculate.h
@@ -0,0 +1,15 @@
+//
+//  calculate.h
+//  makenumber
+//
+//  Created by 海老原祐輔 on 2017/06/26.
+//  Copyright © 2017年 海老原祐輔. All rights reserved.
+//
+
+#ifndef calculate_h
+#define calculate_h
+
+#include "main.h"
+
+
+#endif /* calculate_h */
---
+++ makenumber/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  makenumber
+//
+//  Created by 海老原祐輔 on 2017/06/26.
+//  Copyright © 2017年 海老原祐輔. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+
+
+#endif /* main_h */
